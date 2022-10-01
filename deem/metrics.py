0f3aa472634a320e4ffb14c7a4bd18200a9c7aa5
vaibhavpawar05 <vaibhavpawar05@gmail.com>
2022-10-01 17:57:54 +0530

    Update metrics.py

--- deem/metrics.py
+++ deem/metrics.py
@@ -22,22 +22,15 @@ def evaluate_metrics(user_embs,
             max_topk = max(max_topk, int(metric.split("k=")[-1].strip(")")))
         except:
             raise NotImplementedError('metrics={} not implemented.'.format(metric))
-    
-    if parallel:
-        num_workers = 28
-        with ProcessPoolExecutor(max_workers=num_workers) as executor:
-            #chunk_size = int(np.ceil(len(user_embs) / float(num_workers)))
-            chunk_size = 500
-            tasks = []
-            for idx in range(0, len(user_embs), chunk_size):
-                chunk_user_embs = user_embs[idx: (idx + chunk_size), :]
-                chunk_query_indexes = query_indexes[idx: (idx + chunk_size)]
-                tasks.append(executor.submit(evaluate_block, chunk_user_embs, item_embs, chunk_query_indexes,
-                                             train_user2items, valid_user2items, metric_callers, max_topk))
-            results = [res for future in tqdm(as_completed(tasks), total=len(tasks), disable=True) for res in future.result()]
-    else:
-        results = evaluate_block(user_embs, item_embs, query_indexes, train_user2items, 
-                                 valid_user2items, metric_callers, max_topk)
+
+    chunk_size = 10000
+    results = []
+    for idx in range(0, len(user_embs), chunk_size):
+        chunk_user_embs = user_embs[idx: (idx + chunk_size), :]
+        chunk_query_indexes = query_indexes[idx: (idx + chunk_size)]
+        chunk_results = evaluate_block(chunk_user_embs, item_embs, chunk_query_indexes, train_user2items, valid_user2items, metric_callers, max_topk)
+        results.extend(chunk_results)
+
     average_result = np.average(np.array(results), axis=0).tolist()
     return_dict = dict(zip(metrics, average_result))
     logging.info('[Metrics] ' + ' - '.join('{}: {:.6f}'.format(k, v) for k, v in zip(metrics, average_result)))
@@ -46,9 +39,9 @@ def evaluate_metrics(user_embs,
 def evaluate_block(chunk_user_embs, item_embs, chunk_query_indexes, train_user2items, 
                    valid_user2items, metric_callers, max_topk):
     sim_matrix = np.dot(chunk_user_embs, item_embs.T)
-    for i, query_index in enumerate(chunk_query_indexes):
-        train_items = train_user2items[query_index]
-        sim_matrix[i, train_items] = -np.inf # remove clicked items in train data
+    #for i, query_index in enumerate(chunk_query_indexes):
+    #    train_items = train_user2items[query_index]
+    #    sim_matrix[i, train_items] = -np.inf # remove clicked items in train data
     item_indexes = np.argpartition(-sim_matrix, max_topk)[:, 0:max_topk]
     sim_matrix = sim_matrix[np.arange(item_indexes.shape[0])[:, None], item_indexes]
     sorted_idxs = np.argsort(-sim_matrix, axis=1)
@@ -57,7 +50,6 @@ def evaluate_block(chunk_user_embs, item_embs, chunk_query_indexes, train_user2i
     chunk_results = [[fn(topk_items, true_items) for fn in metric_callers] \
                      for topk_items, true_items in zip(topk_items_chunk, true_items_chunk)]
     return chunk_results
-
 
 class Recall(object):
     """Recall metric."""
